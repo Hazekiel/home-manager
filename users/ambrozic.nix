89a5bbeaae56a93c717fbecb818af67688e14f0b
Hazekiel <239578063+Hazekiel@users.noreply.github.com>
2025-11-05 00:48:53 +0100

    deleting things not usefull

--- users/ambrozic.nix
+++ users/ambrozic.nix
@@ -12,7 +12,6 @@
   home.homeDirectory = "/home/ambrozic";
 
   nixpkgs.config.allowUnfree = true;
-
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
