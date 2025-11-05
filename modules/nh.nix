853a99d384386936152fba244b7b637190cce289
Hazekiel <239578063+Hazekiel@users.noreply.github.com>
2025-11-05 10:02:54 +0100

    nh.nix formatage

--- modules/nh.nix
+++ modules/nh.nix
@@ -1,16 +1,17 @@
 {
-  pkgs, lib,
+  pkgs,
+  lib,
   ...
 }:
 {
-programs.nh = {
-  enable = true;
-  flake = "/etc/nixos";
-  clean = {
+  programs.nh = {
     enable = true;
-    dates = "daily";
-    extraArgs = "--keep-since 2d --keep 3";
+    flake = "/etc/nixos";
+    clean = {
+      enable = true;
+      dates = "daily";
+      extraArgs = "--keep-since 2d --keep 3";
+    };
   };
-};
-systemd.services.nh-clean.serviceConfig.Type=lib.mkForce "simple";
+  systemd.services.nh-clean.serviceConfig.Type = lib.mkForce "simple";
 }
